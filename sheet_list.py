23b92d768238170d6a4c5497777c64194e6ea5c9
lian1803 <lian1803@gmail.com>
2025-07-09 03:46:33 +0900

    불필요한 테스트 파일들 제거 - 깔끔한 배포용 코드만 유지

--- sheet_list.py
+++
@@ -1,6 +0,0 @@
-import openpyxl
-wb = openpyxl.load_workbook('와석초 카카오톡 챗봇 개발을 위한 질문과 답변(0702).xlsx')
-with open('sheets.txt', 'w', encoding='utf-8') as f:
-    for name in wb.sheetnames:
-        f.write(name + '\n')
-wb.close() 
